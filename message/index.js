f4450629df36f920b8e221bee9eb1e0156d966dc
samacgill <68750697+samacgill@users.noreply.github.com>
2021-03-07 10:51:56 +0000

    testing msg app

---
+++ message/index.js
@@ -0,0 +1,21 @@
+const { hash } = window.location;
+
+const message = atob(hash.replace("#", ""));
+if (message) {
+  document.querySelector("#message-form").classList.add("hide");
+  document.querySelector("#message-show").classList.remove("hide");
+  document.querySelector("h1").textContent = message;
+}
+
+document.querySelector("form").addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  document.querySelector("#message-form").classList.add("hide");
+  document.querySelector("#link-form").classList.remove("hide");
+
+  const input = document.querySelector("#message-input");
+  const encrypted = btoa(input.value);
+  const link = document.querySelector("#link-input");
+  link.value = `${window.location}#${encrypted}`;
+  link.select();
+});
